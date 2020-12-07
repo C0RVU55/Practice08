3aae690a2bb06c18644b2db86aa506e8ae630f51
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-08 00:54:47 +0900

    예외처리_String-class

--- src/com/javaex/ex03/Point.java
+++ src/com/javaex/ex03/Point.java
@@ -1,15 +1,20 @@
 package com.javaex.ex03;
 
 public class Point {
-	
+
 	private int x;
 	private int y;
-	
+
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	//toString()을 작성 하세요
+	// toString()을 작성 하세요
+	// Point에서 toString을 재정의하지 않으면 Object에 있는 toString을 호출하게 돼서 원하는 값이 아닌 주소가 출력됨.
+	@Override
+	public String toString() {
+		return "[포인트] x:" + x + ", y:" + y + " 입니다.";
+	}
 
 }
--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class SumApp {
 
 	public static void main(String[] args) {
-		
+
 		int sum = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 공백으로 구분해서 입력하세요");
 		String numLine = sc.nextLine();
 
-		//코드를 작성하세요
-		
-		
-		
+		// 코드를 작성하세요
+		String[] nArr = numLine.split(" "); // 숫자를 공백으로 나눠서 배열에 넣음.
+
+		for (int i = 0; i < nArr.length; i++) {
+			sum = sum + Integer.parseInt(nArr[i]); // String배열에 들어간 숫자를 정수로 형변환해서 합계 구함.
+		}
+
 		System.out.println("합:" + sum);
-		
+
 		sc.close();
 	}
 
