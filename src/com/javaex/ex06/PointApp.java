6ba454f75418f275cc40159007b9d7594ad6e677
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-08 18:18:13 +0900

    예외처리_String-class

--- src/com/javaex/ex06/PointApp.java
+++ src/com/javaex/ex06/PointApp.java
@@ -4,17 +4,20 @@ public class PointApp {
 
 	public static void main(String[] args) {
 		
-		Point p1 = new Point(2, 3);
-		Point p2 = new Point(2, 3);
-		Point p3 = new Point(5, 3);
-		Point p4 = p1;
+		Point p1 = new Point(2, 3); //새 주소값
+		Point p2 = new Point(2, 3); //새 주소값
+		Point p3 = new Point(5, 3); //새 주소값
+		Point p4 = p1; //p4에 p1의 주소값 대입
 		
-		System.out.println(p1 == p2);
-		System.out.println(p2 == p3);
-		System.out.println(p3 == p4);
-		System.out.println(p4 == p1);		
-		System.out.println(p1.equals(p2));
+		System.out.println(p1 == p2); //주소값 비교 --> 다름
+		System.out.println(p2 == p3); //다름
+		System.out.println(p3 == p4); //다름
+		System.out.println(p4 == p1); //같음		
+		System.out.println(p1.equals(p2)); 
 		System.out.println(p4.equals(p1));
+		
+		//Point에서 equals()를 재정의하지 않았기 때문에 Object에 있는 equals 갖다 씀. 
+		//"값이 같으면 같다"로 정의돼 있지 않아서 p1과 p2는 다른 걸로 처리됨.
 	}
 
 }
--- src/com/javaex/ex07/Point.java
+++ src/com/javaex/ex07/Point.java
@@ -1,16 +1,38 @@
 package com.javaex.ex07;
 
 public class Point {
-	
+
 	private int x;
 	private int y;
-	
+
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	//equals() 메소드를 작성하세요.
+	// equals() 메소드를 작성하세요.
 
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (this.x != other.x && this.y == other.y) //해시코드 비교 빼고 여기에는 false조건 추가함.
+			return false;
+		return true;
+	}
+
 }
